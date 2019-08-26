2f66cffd87859fd12aaf6f3d544d45c85f64c7ee
Aleh Dzenisiuk <aleh@mediamonks.com>
2019-08-26 20:37:13 +0200

    Initial example (basic test only)

---
+++ Example/MMMArrayChangesExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+// MMMArrayChanges.
+// Copyright (C) 2019 MediaMonks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
